7297866fdca63316cf1683d074c88456b1697eaf
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2019-12-20 23:25:53 +0900

    add filters and computed

--- app.js
+++ app.js
@@ -20,6 +20,25 @@ var vm = new Vue({
     el: '#app',
     data: {
         items: items
+    },
+    filters: {
+        numberWithDelimeter: function (value) {
+            if (!value){
+                return '0'
+            }
+            return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
+        }
+    },
+    computed: {
+        totalPrice: function () {
+            return this.items.reduce(function (sum, item) {
+                return sum + (item.price * item.quantity)
+            }, 0)
+        },
+        totalPriceWithTax: function () {
+            return Math.floor(this.totalPrice * 1.08)
+
+        }
     }
 })
 
